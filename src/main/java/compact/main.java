31acdd060159398a9502b768e728a122adcadc9d
DiveFish <DiveFish@users.noreply.github.com>
2017-02-21 12:36:45 +0100

    Change to command line input

--- src/main/java/compact/main.java
+++ src/main/java/compact/main.java
@@ -10,18 +10,28 @@ import java.io.IOException;
 public class main {
     
     public static void main(String[] args) throws IOException {
+        
+        String corpus;
+        File directory;
         /*
         String corpus = "taz";
         File directory = new File("/home/patricia/NetBeansProjects/ParliamentDebateTopics/taz_samples/");
         //File directory = new File("/home/patricia/NetBeansProjects/ParliamentDebateTopics/taz/");
         
         */
-        String corpus = "PolMine";
+        corpus = "PolMine";
         //File directory = new File("/home/patricia/NetBeansProjects/ParliamentDebateTopics/bundesparser-xml-tokenized/");
         
-        File directory = new File("/home/patricia/NetBeansProjects/ParliamentDebateTopics/bundesparser-xml-tokenized-samples/");
+        directory = new File("/home/patricia/NetBeansProjects/ParliamentDebateTopics/bundesparser-xml-tokenized-samples/");
         //File directory = new File("/home/patricia/NetBeansProjects/ParliamentDebateTopics/testFiles/");
         
+        
+        
+        if (args.length != 2) {
+            System.out.println("Wrong number of arguments.Usage: 2, provide name of dataset (Polmine/Taz) path to data files");
+        }
+        corpus = args[0].toLowerCase();
+        directory = new File(args[1]);
         
         MatrixBuilder mb = new MatrixBuilder();
         mb.buildMatrix(corpus, directory);
