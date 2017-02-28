596266d4ea5780edcdb440120f3a5971b94c8bef
DiveFish <DiveFish@users.noreply.github.com>
2017-02-28 11:05:46 +0100

    Add "choose best clustering"

--- src/main/java/compact/KMeansClustering.java
+++ src/main/java/compact/KMeansClustering.java
@@ -2,11 +2,10 @@ package compact;
 
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import static matrix.VectorMeasures.euclideanDistance;
+import java.util.*;
+
 import org.la4j.Vector;
 import org.la4j.Matrix;
 import org.la4j.vector.SparseVector;
@@ -17,65 +16,86 @@ import org.la4j.vector.SparseVector;
  * @author Patricia Fischer
  */
 public class KMeansClustering {
-    
+
     private final int numOfClusters;
-    
+
     private final Matrix documentVectors;
+
+    private final Random random;
     
-    
-    public KMeansClustering(int numOfClusters, Matrix documentVectors) {
+    private static double cosine;
+
+
+    public KMeansClustering(int numOfClusters, Matrix documentVectors, Random random) {
         this.numOfClusters = numOfClusters;
         this.documentVectors = documentVectors;
+        this.random = random;
     }
-    
+
     /**
-     * Create several clusters to decide for best one.
+     * Create several clusters to decide for best one based on cosine similarity
+     * of vectors to centroids.
      *
      * @param n Number of cluster calculations
-     * @return
+     * @return Best clustering
      * @throws IOException
      */
-    public List<List<TIntList>> nClusters(Integer n) throws IOException {
-        //TODO: pick best cluster; calculate best centroids and then retrieve clusters?
+    public List<TIntList> nClusters(Integer n) throws IOException {
+        /*
         List<List<TIntList>> clusterColl = new ArrayList();
         for (int i = 0; i < n; i++) {
             clusterColl.add(clusters(centroids()));
         }
-        return clusterColl;
+        */
+        double maximum = -Double.MAX_VALUE;
+        List<TIntList> bestCluster = new ArrayList<>();
+        List<TIntList> cluster;
+        for (int i = 0; i < n; i++) {
+            cluster = clusters(centroids());
+            if (cosine > maximum) {
+                maximum = cosine;
+                bestCluster = cluster;
+            }
+        }
+                
+        return bestCluster;
     }
-    
+
     /**
      * Calculate clusters for matrix from a given list of centroids. Compare
      * the vectors in the matrix to the centroids and assign vectors to closest
-     * centroid.
+     * centroid. Each cluster contains row indices of the doc vectors in the cluster.
      *
      * @param centroids The list of centroid vectors
-     * @return The clusters as list of vector lists
+     * @return The clusters as list of vector idxs
      */
     public List<TIntList> clusters(List<Vector> centroids) {
-        
+
         List<TIntList> clusters = new ArrayList<>(centroids.size());
         for (int i = 0; i < numOfClusters; i++) {
             clusters.add(new TIntArrayList());
         }
-        
-        for (int row = 0; row < documentVectors.rows(); row++) {
-            double minimum = Double.MAX_VALUE;
-            Vector closestCentroid = SparseVector.zero(documentVectors.columns());
 
-            for (Vector centroid : centroids) {
-                double euc = euclideanDistance(documentVectors.getRow(row), centroid);
-                if (euc < minimum) {
-                    minimum = euc;
-                    closestCentroid = centroid;
+        for (int row = 0; row < documentVectors.rows(); row++) {
+            double maxSimilarity = -Double.MAX_VALUE;
+            int closesCentroidIdx = 0;
+
+            for (int i = 0; i < centroids.size(); i++) {
+                Vector centroid = centroids.get(i);
+
+                double similarity = documentVectors.getRow(row).innerProduct(centroid);
+                if (similarity > maxSimilarity) {
+                    maxSimilarity = similarity;
+                    closesCentroidIdx = i;
                 }
             }
-            clusters.get(centroids.indexOf(closestCentroid)).add(row);
+
+            clusters.get(closesCentroidIdx).add(row);
         }
-        
+
         return clusters;
     }
-    
+
     /**
      * Cluster data by k-means clustering. Calculate euclidean distance from all
      * document vectors to all centroids, assign each to their closest centroid.
@@ -84,82 +104,90 @@ public class KMeansClustering {
      * @return The clusters
      * @throws java.io.IOException
      */
-    
-    public List<Vector> centroids() throws IOException {    
-        
+
+    public List<Vector> centroids() throws IOException {
+
         if (documentVectors.max() == 0) {
             throw new IOException("Trying to extract clusters from zero matrix");
         }
-        
+
         if (documentVectors.rows() < numOfClusters) {
             throw new IOException("Trying to extract more clusters than matrix has elements");
         }
-        
+
         int vector_length = documentVectors.columns();  //vocab size
         int num_of_docs = documentVectors.rows();
-        
-        List<Vector> centroids = new ArrayList<>();
-        
+
+        normalizeDocumentVectors(documentVectors);
+
         // Choose random centroids from document vectors
-        Random rand = new Random(28);
-        while (centroids.size() < numOfClusters) {
-            int randomRow = rand.nextInt(num_of_docs);
-            if (!centroids.contains(documentVectors.getRow(randomRow))) {
-                centroids.add(documentVectors.getRow(randomRow));
+        Set<Integer> seedDocs = new HashSet<>();
+        while (seedDocs.size() < numOfClusters) {
+            seedDocs.add(this.random.nextInt(num_of_docs));
+        }
+
+        List<Vector> centroids = new ArrayList<>();
+        for (int doc : seedDocs) {
+            centroids.add(documentVectors.getRow(doc));
+        }
+
+        normalizeDocumentVectors(documentVectors);
+
+        for (int iter = 0; iter < 3; iter++) {
+            double objective = 0;
+
+            int[] numOfClusterElements = new int[numOfClusters];
+            List<Vector> adjustedCentroids = new ArrayList<>();
+            for (int i = 0; i < numOfClusters; i++) {
+                adjustedCentroids.add(SparseVector.zero(vector_length));
             }
-        }
-        
-        // parallel indexing for centroids, adjustedCentroids and numOfClusterElements
-        // -> centroid; doc vectors closest to this centroid; number of doc vectors assigned to centroid
-        List<Vector> adjustedCentroids = new ArrayList();
-        int[] numOfClusterElements = new int[numOfClusters];  
-        
-        while (adjustedCentroids.size() < numOfClusters) {
-            adjustedCentroids.add(SparseVector.zero(vector_length));
-        }
-        
-        boolean converged = false;
-        while (!converged) {
-            
+
+            System.err.println("iteration...");
+
             // Assign vectors to their closest centroid
             for (int row = 0; row < num_of_docs; row++) {
-                double minimum = Double.MAX_VALUE;
-                
+
+                double maximum = -Double.MAX_VALUE;
                 int idx = -1;
                 for (int i = 0; i < centroids.size(); i++) {
-                    double euc = euclideanDistance(documentVectors.getRow(row), centroids.get(i));
-                    if (euc < minimum) {
-                        minimum = euc;
-                        idx = i; // index of closest centroid 
+                    double similarity = documentVectors.getRow(row).innerProduct(centroids.get(i));
+                    if (similarity > maximum) {
+                        maximum = similarity;
+                        idx = i; // index of closest centroid
                     }
                 }
                 // add vector at idx of closest centroid -> "assign" vector to centroid
                 adjustedCentroids.set(idx, adjustedCentroids.get(idx).add(documentVectors.getRow(row)));
                 numOfClusterElements[idx]++;
+                objective += maximum;
             }
-            
-            // Distance between old and recomputed centroids
-            double distOfCentr = 0;
+
+            System.err.println("Recomputing centroids...");
+
             for (int idx = 0; idx < adjustedCentroids.size(); idx++) {
-                
-                // divide sum of cluster vectors by number of vectors in this cluster
-                adjustedCentroids.set(idx, adjustedCentroids.get(idx).divide(numOfClusterElements[idx]));
-                
-                distOfCentr += euclideanDistance(centroids.get(idx), adjustedCentroids.get(idx));   
-                
-                //centroids is now adjustedCentroids; centroids = new arraylist
-                //create empty centroid list at beginning of each loop and fill it in loop
-                //next iteration of loop
-                centroids.set(idx, adjustedCentroids.get(idx)); // make adjusted centroid the new centroid
-                adjustedCentroids.set(idx, SparseVector.zero(vector_length));
+                // Compute centroid and normalize to unit vector.
+                SparseVector unnormalizedCentroid = (SparseVector) adjustedCentroids.get(idx).divide(numOfClusterElements[idx]);
+                adjustedCentroids.set(idx, unnormalizedCentroid.divide(norm(unnormalizedCentroid)));
             }
-            
-            if (distOfCentr/centroids.size() < 10) {
-                converged = true;
-            }
+
+            centroids = adjustedCentroids;
+            cosine = objective / documentVectors.rows();
+            System.err.printf("Average cosine similarity: %s%n", objective / documentVectors.rows());
         }
-        
+
         return centroids;
     }
-    
+
+    private double norm(SparseVector v) {
+        // Euclidean norm in la4j uses BigDecimal, which has a lot of overhead.
+        return Math.sqrt(v.innerProduct(v));
+    }
+
+    private void normalizeDocumentVectors(Matrix documentVectors) {
+        for (int i = 0; i < documentVectors.rows(); i++) {
+            SparseVector doc = (SparseVector) documentVectors.getRow(i);
+            documentVectors.setRow(i, doc.divide(norm(doc)));
+        }
+    }
+
 }
