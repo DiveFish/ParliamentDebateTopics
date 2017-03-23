8fdc7d85e0df5444e332654a57ceb66e55f5fd04
DiveFish <DiveFish@users.noreply.github.com>
2017-03-23 18:50:33 +0100

    Add comment to KMeans

--- src/main/java/ujmp_trial/KMeansClustering.java
+++ src/main/java/ujmp_trial/KMeansClustering.java
@@ -212,6 +212,7 @@ public class KMeansClustering {
         double b2Sum = 0;
 
         // change to nonZeroCoordinates/availableCoordinates
+
         for (long[] c : m1.allCoordinates()) {
             double a = m1.getAsDouble(c);
             double b = m2.getAsDouble(c);
